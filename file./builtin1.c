d3caf9d79b263f2a2e282be32066db1742904159
Weldon Kiprop <118200102+Lagat100@users.noreply.github.com>
2024-01-29 23:34:49 +0300

    Update builtin1.c

--- file./builtin1.c
+++ file./builtin1.c
@@ -1,12 +1,5 @@
 #include "shell.h"
 
-/**
- * display_history - displays the history list, one command per line, preceded
- *                   with line numbers, starting at 0.
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
- */
 int display_history(info_t *info)
 {
     print_list(info->history);
