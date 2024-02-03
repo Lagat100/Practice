f5b4205fa4bf5057d8bff648836cf9ffe488be12
Weldon Kiprop <118200102+Lagat100@users.noreply.github.com>
2024-02-03 22:49:14 +0300

    Update environ.c

--- file./alone/environ.c
+++ file./alone/environ.c
@@ -34,13 +34,7 @@ char *_getenv(info_t *info, const char *name)
 	return (NULL);
 }
 
-/**
- * _mysetenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
- */
+
 int _mysetenv(info_t *info)
 {
 	if (info->argc != 3)
