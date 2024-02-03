63a68c2ee9f12eca83930b125615c09a9140bb55
Weldon Kiprop <118200102+Lagat100@users.noreply.github.com>
2024-02-03 22:47:39 +0300

    Update _atoi.c

--- file./alone/_atoi.c
+++ file./alone/_atoi.c
@@ -35,11 +35,7 @@ int _isalpha(int c)
 		return (0);
 }
 
-/**
- *_atoi - converts a string to an integer
- *@s: the string to be converted
- *Return: 0 if no numbers in string, converted number otherwise
- */
+
 
 int _atoi(char *s)
 {
