41f30f24b34031e755bc5655defe7a8143628f5f
Weldon Kiprop <118200102+Lagat100@users.noreply.github.com>
2024-02-05 23:53:54 +0300

    Update shell_loop.c

--- file./shell_loop.c
+++ file./shell_loop.c
@@ -45,15 +45,7 @@ int hsh(shell_info_t *info, char **av)
 	return (builtin_ret);
 }
 
-/**
- *  * find_builtin - Finds a builtin command.
- *   * @info: The parameter & return info struct.
- *    *
- *     * Return: -1 if builtin not found,
- *      * 0 if builtin executed successfully,
- *       * 1 if builtin found but not successful,
- *        * -2 if builtin signals exit().
- *         */
+
 int find_builtin(shell_info_t *info)
 {
 	int i, built_in_ret = -1;
