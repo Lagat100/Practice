39d161992a66b82887947796e14e221f208b5329
Weldon Kiprop <118200102+Lagat100@users.noreply.github.com>
2024-02-18 23:53:17 +0300

    Update history.c

--- file./history.c
+++ file./history.c
@@ -111,12 +111,6 @@ int build_history_list(info_t *info, char *buf, int line_count)
 	return (0);
 }
 
-/**
- * renumber_history - renumbers the history linked list after changes
- * @info: Structure containing potential arguments. Used to maintain
- *
- * Return: the new histcount
- */
 int renumber_history(info_t *info)
 {
 	list_t *node = info->history;
