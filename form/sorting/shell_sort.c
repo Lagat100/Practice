8adc92761321e5329f59d22086a83e5d772a45b8
Weldon Kiprop <118200102+Lagat100@users.noreply.github.com>
2024-07-18 23:59:06 +0300

    Update shell_sort.c

--- form/sorting/shell_sort.c
+++ form/sorting/shell_sort.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "sort.h"
 
-
 int main(void)
 {
 	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
@@ -15,3 +14,4 @@ int main(void)
 	print_array(array, n);
 	return (0);
 }
+
